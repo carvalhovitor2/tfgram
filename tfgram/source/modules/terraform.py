d92af317acdf2f971769b7eb82b40314eddb12be
Vitor Carvalho <vac@eurotux.com>
2023-04-08 21:58:57 +0100

    Adding module directory and renaming validate functiont

---
+++ tfgram/source/modules/terraform.py
@@ -0,0 +1,24 @@
+import os
+import hcl
+
+class TerraformParser:
+    def __init__(self, files):
+        if not isinstance(files, list):
+            files = [files]
+        
+        self.files = files
+
+    def validate(self):
+        for file in self.files:
+            if not os.path.exists(file):
+                raise FileNotFoundError(f"File not found: {file}")
+
+            if not file.endswith('.tf'):
+                raise ValueError(f"Not a Terraform file: {file}")
+
+            with open(file, 'r') as f:
+                try:
+                    hcl.load(f)
+                except hcl.parser.ParseError as e:
+                    raise ValueError(f"Invalid Terraform file ({file}): {e}")
+
